4e5e58b810dc24d8cb198fc464c5844a4806a245
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-21 21:14:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,12 @@
 def nyc_pigeon_organizer(data)
-  pigion_list = {}
+  pigeon_list = {}
   data.each do |color_gender_lives, hash|
     color_gender_lives.each do |attribute, pigeons|
       pigeons.each do |name|
-        
+        if pigeon_list[name]
+          
+        else
+        end
       end
     end 
   end
