51bfad1b14a1b882cfb13f39d114fd6816cb40c6
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-21 20:49:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,8 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigion_list = {}
+  data.each do |first_keys, first_attributes|
+    first_keys.each do |second_keys, second_attributes|
+      
+    end 
+  end 
 end
