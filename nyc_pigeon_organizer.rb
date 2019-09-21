516f5dbc077ac756964985702669a6c84f8ff0d1
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-21 21:27:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,7 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  #go through data and grab the color/gender/lives key and their hash
   data.each do |color_gender_lives, hash|
-    #go through each of the associated hashes attached to the color/gender/lives key and get the statistics and pigeon names 
     hash.each do |stats, pigeons|
-      #for each of the pigeons in the associated array 
       pigeons.each do |name|
         if pigeon_list[name] == nil
           pigeon_list[name] = {}
